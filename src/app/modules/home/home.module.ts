56fac59e766c29d71bc1a09dd1e88c083fa78391
Renan lucilio <RSL1219>
2020-06-19 08:59:41 -0300

    Ajusts de layout

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class HomeModule { }
