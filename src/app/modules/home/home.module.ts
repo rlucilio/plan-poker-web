75ab65f9629defa448e3ab270cd6871d860d0c5e
Renan lucilio <RSL1219>
2020-06-22 08:43:01 -0300

    Ajustando layout

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,9 +9,11 @@ import { FormRoomComponent } from './components/form-room/form-room.component';
 import { RoomGatewayService } from 'src/app/shared/services/room-gateway/room-gateway.service';
 import { CreateRoomUsecaseService } from './services/room/usecase/create-room.usecase.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EnterRoomComponent } from './components/enter-room/enter-room.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
 @NgModule({
-  declarations: [HomeComponent, FormRoomComponent],
+  declarations: [HomeComponent, FormRoomComponent, EnterRoomComponent, CreateRoomComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
