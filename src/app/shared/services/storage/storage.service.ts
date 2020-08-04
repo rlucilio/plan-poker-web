78826e0900550cfdc3116bae0762cd0fe695cef9
Renan lucilio <RSL1219>
2020-08-04 08:13:53 -0300

    fix

--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-  private storage = window.sessionStorage;
+  private storage = window.localStorage;
   constructor() { }
 
   setValue(key: string, value: any) {
