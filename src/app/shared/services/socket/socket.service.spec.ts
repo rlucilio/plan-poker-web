0f45779fb6c784f9ab9e4ab17645039b8d2882ea
Renan lucilio <RSL1219>
2020-08-18 09:03:46 -0300

    ajustes de layout

--- src/app/shared/services/socket/socket.service.spec.ts
+++ src/app/shared/services/socket/socket.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
-
 import { SocketService } from './socket.service';
+
+
 
 describe('SocketService', () => {
   let service: SocketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SocketService);
+    jest.resetAllMocks();
   });
 
+  const createComponent = () => {
+    service = new SocketService();
+  };
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    createComponent();
+    expect(service).toBeDefined();
   });
 });
--- src/app/shared/services/storage/storage.service.spec.ts
+++ src/app/shared/services/storage/storage.service.spec.ts
@@ -43,12 +43,64 @@ describe('StorageService', () => {
 
   it('verify method setObject error', () => {
     const key = '';
-    const value = { prop: 'value' };
+    const value =  null;
 
     createService();
 
     expect(() => {
       storageService.setObject(key, value);
     }).toThrow('Params invalid');
+  });
+
+  it('verify method getObject', () => {
+    const key = 'key';
+    window.sessionStorage.setItem(key, JSON.stringify({ prop: 'value' }));
+
+    createService();
+    const result = storageService.getObject(key);
+
+    expect(result).toEqual({ prop: 'value' });
+  });
+
+  it('verify method getObject error', () => {
+    const key = '';
+    window.sessionStorage.setItem(key, JSON.stringify({ prop: 'value' }));
+
+    createService();
+
+    expect(() => {
+      storageService.getObject(key);
+    }).toThrowError('key invalid');
+  });
+
+  it('verify method getValue', () => {
+    const key = 'key';
+    window.sessionStorage.setItem(key, 'value' );
+
+    createService();
+
+    const result = storageService.getValue(key);
+
+    expect(result).toEqual('value');
+  });
+
+  it('verify method getValue error', () => {
+    const key = '';
+    window.sessionStorage.setItem(key, 'value' );
+
+    createService();
+
+    expect(() => {
+      storageService.getValue(key);
+    }).toThrowError('key invalid');
+  });
+
+  it('verify method clear', () => {
+    window.sessionStorage.setItem('key', 'value' );
+
+    createService();
+    storageService.clear();
+
+    expect(window.sessionStorage.length).toEqual(0);
   });
 });
